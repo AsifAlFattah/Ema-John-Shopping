3eaf1adbd2742aa9249c295b6ffadc38589de35f
Asif Al Fattah <asif.alfattah.2@gmail.com>
2023-01-14 15:36:44 +0600

    Cart Calculation

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -2,11 +2,26 @@ import React from 'react';
 import './Cart.css';
 
 const Cart = ({cart}) => {
+    let total = 0;
+    let shipping = 0;
+    let quantity = 0;
+    for(const product of cart){
+        quantity = quantity + product.quantity;
+        total = total + product.price * product.quantity;
+        shipping = shipping + product.shipping;
+    }
+
+    const tax = Math.round(total * 0.05);
+
     return (
-        <div>
+        <div className='cart'>
             <h2>Order Summary</h2>
             <div className='cart-content'>
-                <p>Selected Items: {cart.length}</p>
+                <p>Selected Items: {quantity}</p>
+                <p>Total Price: ${total}</p>
+                <p>Total Shipping Charge: ${shipping}</p>
+                <p>Tax: ${tax}</p>
+                <h4>Grand Total: ${total+shipping+tax}</h4>
             </div>
             
         </div>
