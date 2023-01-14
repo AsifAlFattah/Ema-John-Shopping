db3475a15867516128b8263dc8691d20dd5b985f
Asif Al Fattah <asif.alfattah.2@gmail.com>
2023-01-14 17:05:14 +0600

    Clear Cart

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,7 +1,12 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
 import './Cart.css';
 
-const Cart = ({cart}) => {
+const Cart = ({cart,clearCart}) => {
+
+    
+
     let total = 0;
     let shipping = 0;
     let quantity = 0;
@@ -13,6 +18,8 @@ const Cart = ({cart}) => {
 
     const tax = Math.round(total * 0.05);
 
+    
+
     return (
         <div className='cart'>
             <h2>Order Summary</h2>
@@ -23,7 +30,10 @@ const Cart = ({cart}) => {
                 <p>Tax: ${tax}</p>
                 <h4>Grand Total: ${total+shipping+tax}</h4>
             </div>
-            
+            <div className='btn-grp'>
+                <button className='clear-btn' onClick={clearCart}><p>Clear Cart &nbsp;<FontAwesomeIcon icon={faTrash}></FontAwesomeIcon></p></button> <br/ >
+                <button className='review-btn' onClick={clearCart}><p>Place Order &nbsp;<FontAwesomeIcon icon={faArrowRight}></FontAwesomeIcon></p></button>
+            </div>
         </div>
     );
 };
